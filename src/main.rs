681cf2d076b2c6f268e0e356512e29b9854bf8ac
chisommaduabuchi8 <chisommaduabuchi8@gmail.com>
2024-12-15 00:48:33 +0000

    messag3

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let mut rng = rand::thread_rng();
 
     // Generate a random number between 1 and 100
-    let random_number: u64 = rng.gen_range(9999999999999999..=99999999990);
+    let random_number: u64 = rng.gen_range(1..=900);
+    println!("Random message")
     println!("Random number: {}", random_number);
 }
